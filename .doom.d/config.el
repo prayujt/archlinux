4903ffc20f33193538e818f30339cabd083f536a
Prayuj Tuli <82406637+prayujt@users.noreply.github.com>
2023-01-19 22:38:59 -0500

    Changed background and color schemes

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -227,3 +227,40 @@
 ;; ---- ein configuration ---
 
 (setq ein:jupyter-server-args '("--ip" "127.0.0.1"))
+
+(defun insert-jupyter-json ()
+        (interactive "*")
+        (insert
+                "{
+                        \"cells\": [
+                                {
+                                        \"cell_type\": \"code\",
+                                        \"execution_count\": null,
+                                        \"id\": \"b727f3c4\",
+                                        \"metadata\": {},
+                                        \"outputs\": [],
+                                        \"source\": []
+                                }
+                        ],
+                        \"metadata\": {
+                                \"kernelspec\" {
+                                        \"display_name\": \"Python 3 (ipykernel)\",
+                                        \"language\": \"python\",
+                                        \"name\": \"python3\"
+                                },
+                                \"language_info\": {
+                                        \"codemirror_mode\": {
+                                                \"name\": \"ipython\",
+                                                \"version\": 3
+                                        },
+                                        \"file_extension\": \"py\",
+                                        \"mimetype\": \"text/x-python\",
+                                        \"name\": \"python\",
+                                        \"nbconvert_exporter\": \"python\",
+                                        \"pygments_lexer\": \"ipython3\",
+                                        \"version\": \"3.10.9\"
+                                }
+                        },
+                        \"nbformat\": 4,
+                        \"nbformat_minor\": 5
+                }"))
